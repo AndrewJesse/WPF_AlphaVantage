f286d7e08c3d807050185d3b713f51cbe74ed0bd
Andrew J Marchese <aj.marchese@outlook.com>
2023-11-12 10:52:30 -0800

    updated key path

---
+++ AlphaVantage.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+public class AlphaVantage
+{
+    private readonly string _apiKey;
+
+    public AlphaVantage(string apiKey)
+    {
+        _apiKey = apiKey;
+    }
+
+    public async Task<List<(string date, decimal open, decimal high, decimal low, decimal close)>> GetDailyPricesAsync(string symbol)
+    {
+        string queryUrl = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={_apiKey}";
+
+        using (HttpClient client = new HttpClient())
+        {
+            string? jsonString = await client.GetStringAsync(queryUrl);
+
+            var stockData = JsonSerializer.Deserialize<StockData>(jsonString);
+
+            List<(string date, decimal open, decimal high, decimal low, decimal close)> dailyPrices = new List<(string date, decimal open, decimal high, decimal low, decimal close)>();
+
+            if (stockData != null && stockData.TimeSeriesDaily != null)
+            {
+                foreach (var entry in stockData.TimeSeriesDaily)
+                {
+                    if (entry.Value != null)
+                    {
+                        dailyPrices.Add((entry.Key, decimal.Parse(entry.Value.Open), decimal.Parse(entry.Value.High), decimal.Parse(entry.Value.Low), decimal.Parse(entry.Value.Close)));
+                    }
+                }
+            }
+
+            return dailyPrices;
+        }
+    }
+}
+
+public class GetAPI
+{
+    public string? api_key { get; set; }
+}
+
+public class StockData
+{
+    [JsonPropertyName("Meta Data")]
+    public MetaData? MetaData { get; set; }
+
+    [JsonPropertyName("Time Series (Daily)")]
+    public Dictionary<string, DailyQuote>? TimeSeriesDaily { get; set; }
+}
+
+public class MetaData
+{
+    [JsonPropertyName("1. Information")]
+    public string? Information { get; set; }
+
+    [JsonPropertyName("2. Symbol")]
+    public string? Symbol { get; set; }
+
+    [JsonPropertyName("3. Last Refreshed")]
+    public string? LastRefreshed { get; set; }
+
+    [JsonPropertyName("4. Output Size")]
+    public string? OutputSize { get; set; }
+
+    [JsonPropertyName("5. Time Zone")]
+    public string? TimeZone { get; set; }
+}
+
+public class DailyQuote
+{
+    [JsonPropertyName("1. open")]
+    public string? Open { get; set; }
+
+    [JsonPropertyName("2. high")]
+    public string? High { get; set; }
+
+    [JsonPropertyName("3. low")]
+    public string? Low { get; set; }
+
+    [JsonPropertyName("4. close")]
+    public string? Close { get; set; }
+
+    [JsonPropertyName("5. volume")]
+    public string? Volume { get; set; }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,32 @@ namespace WPF_AlphaVantage
     /// </summary>
     public partial class MainWindow : Window
     {
+        private AlphaVantage alphaVantage;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Read and deserialize the API key from the JSON file
+            string keyPath = "C:\\Projects\\WPF_AlphaVantage\\api_key.json";
+            string json = File.ReadAllText(keyPath);
+            var jsonObject = JsonSerializer.Deserialize<GetAPI>(json);
+
+            // Initialize your AlphaVantage with the API key from the file
+            if (jsonObject?.api_key != null)
+            {
+                alphaVantage = new AlphaVantage(jsonObject.api_key);
+            }
+            else
+            {
+            }
+        }
+
+        private async void LoadData_Click(object sender, RoutedEventArgs e)
+        {
+            var data = await alphaVantage.GetDailyPricesAsync("SPY");
+
+            // TODO: Add logic to process and display this data in a chart
         }
     }
 }
